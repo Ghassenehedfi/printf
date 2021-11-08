c516325f94392f0eadc2a65628552d4c95f9c278
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-11-08 19:19:28 +0100

    header

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _printf(const char *format, ...);
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
+int _printf(const char *format, ...);
+int _putchar(char c);
 
 #endif
