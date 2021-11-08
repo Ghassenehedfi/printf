051896f8d201acb52674ab4e8d4b370cda752e60
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2021-11-08 22:54:54 +0100

    header

--- main.h
+++ main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-
-
+#include <stdio.h>
 /**
  * struct printf - struct printf
  * @c: The character
@@ -16,14 +15,16 @@ typedef struct printf
 	char c;
 	int (*fun)(va_list);
 } printf_t;
+/* functions prototype */
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 int get_func(const char s, va_list ap);
 
-int _print_char(va_list arg);
-int _print_string(va_list arg);
-int _print_percent(va_list arg);
-int _print_int(va_list arg);
+/* functions prototypes conversion */
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_int(va_list arg);
 
 #endif
